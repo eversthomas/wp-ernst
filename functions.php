b22edcfde2cad60d4e12b41cce6d52855d35780d
Thomas <evers.thomas@gmail.com>
2021-08-23 14:42:50 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * wp_ernst functions and definitions
+ *
+ * @package wp_ernst
+ * @since wp_ernst 1.0
+ */
+ 
+/**
+ * First, let's set the maximum content width based on the theme's design and stylesheet.
+ * This will limit the width of all uploaded images and embeds.
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 800; /* pixels */
+ 
+if ( ! function_exists( 'wp_ernst_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function wp_ernst_setup() {
+ 
+    /**
+     * Make theme available for translation.
+     * Translations can be placed in the /languages/ directory.
+     */
+    load_theme_textdomain( 'wp_ernst', get_template_directory() . '/languages' );
+ 
+    /**
+     * Add default posts and comments RSS feed links to <head>.
+     */
+    add_theme_support( 'automatic-feed-links' );
+ 
+    /**
+     * Enable support for post thumbnails and featured images.
+     */
+    add_theme_support( 'post-thumbnails' );
+ 
+    /**
+     * Add support for two custom navigation menus.
+     */
+    register_nav_menus( array(
+        'primary'   => __( 'Primary Menu', 'wp_ernst' ),
+        'secondary' => __('Secondary Menu', 'wp_ernst' )
+    ) );
+ 
+    /**
+     * Enable support for the following post formats:
+     * aside, gallery, quote, image, and video
+     */
+    add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+}
+endif; // wp_ernst_setup
+add_action( 'after_setup_theme', 'wp_ernst_setup' );
