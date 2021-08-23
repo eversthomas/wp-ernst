3b78554d9b7e427da2078610acdedece085707b7
Thomas <evers.thomas@gmail.com>
2021-08-23 14:40:19 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <div class="site-footer">
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
