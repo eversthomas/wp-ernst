8eaea4063ded9db00e02dfa09bd621b48a58de06
Thomas <evers.thomas@gmail.com>
2021-08-23 14:37:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php get_header(); ?>
 
+  <main>
+    <article>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+          the_content();
+      endwhile;
+      else :
+          _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+      endif; ?>
+    </article>
+  </main>
+
+  <?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
