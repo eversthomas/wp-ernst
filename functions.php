ea204da75167e643d0192da6bce82bff952a74dd
Thomas <evers.thomas@gmail.com>
2021-08-24 18:37:58 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -58,12 +58,20 @@ add_action( 'after_setup_theme', 'wp_ernst_setup' );
 
 function add_wp_ernst_scripts() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap' );
   
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
       wp_enqueue_script( 'comment-reply' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'add_wp_ernst_scripts' );
+
+// Replaces the excerpt "Read More" text by a link
+function new_excerpt_more($more) {
+       global $post;
+	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Read the full article...</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
 
 // include some hooks
 require_once( get_stylesheet_directory(). '/hooks/hooks.php' );
