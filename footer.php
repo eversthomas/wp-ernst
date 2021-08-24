bd6d2805ea6ed149327c33d90151afd093cc619b
Thomas <evers.thomas@gmail.com>
2021-08-24 18:37:19 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
-  <div class="site-footer">
-    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
-  </div>
+    <div class="site-footer">
+        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp_ernst' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'wp_ernst' ), 'WordPress' ); ?></a>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
