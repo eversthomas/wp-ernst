7a8c2487b623e1ed0bb2b071b21833835b93d96f
Thomas <evers.thomas@gmail.com>
2021-08-24 18:35:30 +0200

    Update hooks.php

--- hooks/hooks.php
+++ hooks/hooks.php
@@ -1,1 +1,16 @@
 <?php
+function blog_list() { ?>
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <?php the_excerpt(); ?>
+    </article>
+<?php }
+add_action( 'front_loop', 'blog_list', 1 );
+
+function full_content() { ?>
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    </article>
+<?php }
+add_action( 'site_content', 'full_content', 1 );
