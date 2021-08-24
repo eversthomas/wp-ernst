6fc5814084447c721233fe3f0764b404e7c14184
Thomas <evers.thomas@gmail.com>
2021-08-24 18:35:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php get_header(); ?>
 
   <main>
-    <article>
-      <?php do_action( 'bloglist' ); ?>
-    </article>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+        
+        if ( is_home() ):
+            do_action( 'front_loop' );
+        else:
+            do_action( 'site_content' );
+        endif;
+        
+    endwhile;
+    else :
+        _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+    endif; ?>
   </main>
 
   <?php get_sidebar(); ?>
