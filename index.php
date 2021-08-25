7ba1e2b79046e528d511634ed6ec4323251423a7
Thomas <evers.thomas@gmail.com>
2021-08-25 13:53:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,26 @@
             do_action( 'front_loop' );
         else:
             do_action( 'site_content' );
+            
+            if ( is_single() ):
+                next_post_link();
+                previous_post_link();
+            else:
+            endif;
+            
         endif;
+        
+        // If comments are open or we have at least one comment, load up the comment template.
+         if ( comments_open() || get_comments_number() ) :
+             comments_template();
+         endif;
         
     endwhile;
     else :
         _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
     endif; ?>
+    
+    <?php posts_nav_link(); ?>
   </main>
 
   <?php get_sidebar(); ?>
